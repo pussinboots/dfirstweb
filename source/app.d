456a456a9ead3252a87bba438e0639a0578063ae
pussinboots <pussinboots666@googlemail.com>
2014-01-01 18:38:31 +0100

    - code cleanup

--- source/app.d
+++ source/app.d
@@ -12,33 +12,29 @@ import std.string;
 import std.getopt;
 
 @rootPathFromName
-interface Example1API
-{        
-        string getSomeInfo();
+interface Example1API 
+{       string getSomeInfo();
 	Stock[] getStocks();
 	Balance[] getBalances();
 }
 
 class Example1 : Example1API
-{
-	 override:
-                string getSomeInfo()
-                {
-			//TODO: added database information and some os information maybe
+{	override:
+                string getSomeInfo() 
+		{	//TODO: added database information and some os information maybe
                         return "{service:'balanceservice', version:'0.0.9'}";
                 }
 
-                Stock[] getStocks()
-                {
-                        auto c = new Connection("host=localhost;user=root;pwd=root;db=stock_manager");
+                Stock[] getStocks() 
+		{	auto c = new Connection("host=localhost;user=root;pwd=root;db=stock_manager");
 			scope(exit) c.close();			
 			auto c1 = Command(c);
 			c1.sql = "select id, name, value, date from stocks";
 			ResultSet rs = c1.execSQLResult();
 			Json.emptyObject;
 			Stock[] stocks;
-		 	foreach (Row row; rs) {
-				Stock stock = new Stock();
+		 	foreach (Row row; rs) 
+			{	Stock stock = new Stock();
 				stock.id = row[0].get!int;
 				stock.name = row[1].toString();
 				stock.value = row[2].get!double;
@@ -47,17 +43,16 @@ class Example1 : Example1API
 		   	}
 			return stocks;
                 }
-		Balance[] getBalances()
-                {
-                        auto c = new Connection("host=localhost;user=root;pwd=root;db=stock_manager");
+		Balance[] getBalances() 
+		{	auto c = new Connection("host=localhost;user=root;pwd=root;db=stock_manager");
 			scope(exit) c.close();			
 			auto c1 = Command(c);
 			c1.sql = "select id, name, value, date from balances";
 			ResultSet rs = c1.execSQLResult();
 			Json.emptyObject;
 			Balance[] balances;
-		 	foreach (Row row; rs) {
-				Balance balance = new Balance();
+		 	foreach (Row row; rs) 
+			{	Balance balance = new Balance();
 				balance.id = row[0].get!int;
 				balance.name = row[1].toString();
 				balance.value = row[2].get!double;
@@ -71,7 +66,6 @@ class Example1 : Example1API
 class Stock {int id; string name; double value; DateTime date;}
 class Balance {int id; string name; double value; DateTime date;}
 shared static this()
-//void main()
 {	auto settings = new HTTPServerSettings;
 	auto routes = new URLRouter;
 	registerRestInterface(routes, new Example1());
@@ -83,8 +77,7 @@ shared static this()
 	logInfo("or on heroku open http://firstd.herokuapp.com/example1_api/some_info in your browser.");
 }
 void main(string[] args)
-{
-	// returns false if a help screen has been requested and displayed (--help)
+{	// returns false if a help screen has been requested and displayed (--help)
 	if (!finalizeCommandLineOptions())
 		return;
 	ushort port = 8080;
