ac081ae96b50b7f45ca4ef7a11bb71598e967c29
pussinboots <pussinboots666@googlemail.com>
2014-02-02 13:07:46 +0100

    - try to fix compile error

--- source/app.d
+++ source/app.d
@@ -41,9 +41,8 @@ interface Example1API
 	BalanceCollection getBalances(DateTime date = DateTime.init, string name = "", string sort = "id desc", int page = 1, int items = 25);
 }
 
-string addAccessControlOrigin(string result, HTTPServerRequest, HTTPServerResponse res)
+void addAccessControlOrigin(HTTPServerRequest, HTTPServerResponse res)
 {	res.headers["Access-Control-Allow-Origin"] = "*";
-	return result;
 }
 
 class Example1 : Example1API
